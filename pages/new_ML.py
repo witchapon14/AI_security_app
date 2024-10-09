75fa4e0045ae840898bcf69c923847db35013d7b
Game38409 <54000918+Game38409@users.noreply.github.com>
2024-10-09 13:55:28 +0700

    Update new_ML.py

--- pages/new_ML.py
+++ pages/new_ML.py
@@ -30,7 +30,7 @@ if uploaded_file is not None:
     st.write("Uploaded CSV Data:", df)
     df = df.drop(columns='Unnamed: 0')
     # โหลดโมเดล Random Forest
-    model = joblib.load(r'pages/random_forest_model.pkl')
+    model = joblib.load('/pages/random_forest_model.pkl')
     predictions = predict_attack(df)
     df['Portscan Prediction'] = predictions
     # ไฮไลท์คอลัมน์ที่เป็น Portscan
